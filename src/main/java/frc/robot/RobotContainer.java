5765e3359a57b4ef404c526bcadaa7ae20039cba
rogerc <roger.z.cui@icloud.com>
2025-02-05 20:33:47 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,6 +55,8 @@ public class RobotContainer {
     // Set the options to show up in the Dashboard for selecting auto modes. If you
     // add additional auto modes you can add additional lines here with
     // autoChooser.addOption
+
+    // This auton hasn NOT been tested!
     autoChooser.setDefaultOption("Simple Coral Auton", m_simpleCoralAuton);
     SmartDashboard.putData(autoChooser);
 
